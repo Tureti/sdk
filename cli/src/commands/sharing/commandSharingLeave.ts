c8418918051630b44e38df8a24feffb7831e2d64
drive <drive@proton.ch>
2026-07-13 08:43:15 +0000

    feat(cli)!: allow leaving shared node by path

--- cli/src/commands/sharing/commandSharingLeave.ts
+++ cli/src/commands/sharing/commandSharingLeave.ts
@@ -3,11 +3,14 @@ import { type ActionArgs, type Command } from '../../cli';
 export class CommandSharingLeave implements Command {
     group = 'sharing';
     name = 'leave';
-    help = 'Leaves a node that was previously shared with you. Get the UID from the shared-with-me listing.';
-    args = ['nodeUid'];
+    help = 'Leaves a node that was previously shared with you.';
+    args = ['path'];
 
-    async action({ sdk, args: [nodeUid], options: { json } }: ActionArgs) {
-        await sdk.leaveSharedNode(nodeUid);
+    async action({ paths, args: [pathString], options: { json } }: ActionArgs) {
+        const nodePath = paths.getPath(pathString);
+        const node = await nodePath.getNode();
+
+        await nodePath.sdk.leaveSharedNode(node);
 
         if (!json) {
             console.log(`✅ Left shared node`);
