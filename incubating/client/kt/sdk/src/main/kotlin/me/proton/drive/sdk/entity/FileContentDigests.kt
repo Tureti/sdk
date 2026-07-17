6d00871fdd6bdf1d8ce16808b690a978de950405
drive <drive@proton.ch>
2026-07-17 07:26:43 +0200

    feat(client-kt): represent sha1 as byte array not string

--- incubating/client/kt/sdk/src/main/kotlin/me/proton/drive/sdk/entity/FileContentDigests.kt
+++ incubating/client/kt/sdk/src/main/kotlin/me/proton/drive/sdk/entity/FileContentDigests.kt
@@ -1,6 +1,24 @@
 package me.proton.drive.sdk.entity
 
 data class FileContentDigests(
-    val sha1: String?,
+    val sha1: ByteArray?,
     val sha1Verified: Boolean,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FileContentDigests
+
+        if (sha1Verified != other.sha1Verified) return false
+        if (!sha1.contentEquals(other.sha1)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sha1Verified.hashCode()
+        result = 31 * result + (sha1?.contentHashCode() ?: 0)
+        return result
+    }
+}
